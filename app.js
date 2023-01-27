12086674e0dbcb8cfe8c7f7558a283c1bb997372
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-26 17:10:54 -0800

    decided to first test some functionality between my supabase table and my app, added event listener on window load to get posts from supabase and my posts are showing up in console

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 /* Imports */
+import { getPosts } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const postsEl = document.getElementById('posts');
+const signInButton = document.getElementById('sign-in-button');
+const createPageButton = document.getElementById('create-page-button');
 
 /* State */
+let postsArray = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const postsData = await getPosts();
+    postsArray = postsData;
+    console.log(postsArray);
+});
 
 /* Display Functions */
 
